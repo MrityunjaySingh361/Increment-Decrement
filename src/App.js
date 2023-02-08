3fa0760edc49f52d4866571f1c1247a8a797726f
mrityunjay singh <mrityunjaysingh22439@gmail.com>
2023-02-08 22:36:38 +0530

    mrityunjay

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./App.css"
+function App(){
+  const [data , setData]= useState(0)
+  return (
+  <div className="meradiv">
+    <h2>My first project here</h2>
+    <h1>{data}</h1>
+ <button onClick={()=>setData (data+1)} >increament </button>
+ <button  onClick={()=>setData (data-1)}>decrememt</button>
+  </div>
+  )
+}
+export default App;
